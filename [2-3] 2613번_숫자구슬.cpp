ca3008762f4d1c25c6310d6c737789fd84033bf4
Jiwon-Hwang <54762704+Jiwon-Hwang@users.noreply.github.com>
2020-03-30 07:02:35 +0900

    2613_Parametric Search, m개그룹개수 맞추기 예외처리!

--- [2-3] 2613번_숫자구슬.cpp
+++ [2-3] 2613번_숫자구슬.cpp
@@ -4,21 +4,36 @@ using namespace std;
 int arr[300] = {0,};
 int groups_marble_num[300] = {0,}; // 각 그룹의 구슬 개수들
 int group_idx = 0; // 총 그룹 개수
-//int group_max = 0;
 
 void count_marble_num(int num, int n, int m) {
-	int rest = num;
-	int j = 0;
-	for (int i = 0; i < n; i++) {
-		if (rest - arr[i] < 0) {
-			rest = num;
-			rest -= arr[i];
-			groups_marble_num[group_idx++] = j;
-			j = 1;
+	int sum = 0;
+	for (int i = 0; i < n; i++) {       
+		if (sum + arr[i] <= num) { // 담을 수 있으면
+			sum += arr[i]; // 담기
+			groups_marble_num[group_idx]++; 
+			//printf("새로 더한 구슬 값 arr[i] : %d, ", arr[i]);
+			//printf("groups_marble_num : %d\n", groups_marble_num[group_idx]);
 			continue;
 		}
-		rest -= arr[i];
-		j++;
+		groups_marble_num[++group_idx]++; // 다음 그룹으로 넘어가면서 개수 1개 추가
+		//printf("group_idx : %d, groups_marble_num : %d\n", group_idx, groups_marble_num[group_idx]);
+		sum = arr[i]; // 9
+	}
+
+	// ***예외처리 부분*** : m개의 그룹보다 덜 쪼개졌을 때, m개 꼭 맞춰서 다시 더 쪼개주기!!
+	int idx = group_idx;
+	for (int i = 0; m - (idx + 1) - i > 0; i++) {
+		for (int j = 0; j <= idx; j++) {
+			if (groups_marble_num[idx - j] == 1) { // 그룹의 구슬 개수가 1개면 넘어가기(뒤부터 확인)
+				continue;
+			} 
+			else { // 2개 이상이면 더 쪼개주기 ==> ***결국 각 그룹의 합이 아닌, '개수'만 구하면 됨!!***				
+				groups_marble_num[idx - j]--;
+				groups_marble_num[++group_idx] = 1;
+				break; // 한 번 쪼개고 나서 바깥 for문 확인(m개의 그룹이 됐는지 확인)
+			}
+		}
+		
 	}
 }
 
@@ -34,8 +49,6 @@ bool isPossible(int mid, int n, int m) {
 			rest = mid;
 			rest -= arr[i];
 			continue; // 못 빼는 경우에도, 여기에서 충전 후 다시 빼줬으므로 밑에는 실행 안하도록 continue
-			//int x = mid - (rest + arr[i]);
-			//group_max = group_max > x ? group_max : x;
 		}
 		// 뺄 수 있으면 빼기 (rest = arr[i] 여도)
 		rest -= arr[i]; 
@@ -58,7 +71,6 @@ int binary_search(int min, int max, int n, int m) {
 		int mid = (min + max) / 2;
 
 		if (isPossible(mid, n, m)) {
-			//mid = group_max;
 			return binary_search(min, mid, n, m);
 		}
 		else {
@@ -79,7 +91,7 @@ int main() {
 	}
 	
 	int result = binary_search(min, max, n, m);
-	printf("%d\n", result);
+	printf("%d \n", result);
 	for (int i = 0; i <= group_idx; i++) {
 		printf("%d ", groups_marble_num[i]);
 	}
